229f96349223f3ced89d5a3afd678de97b83a92f
Naveen Prabakar <123602587+NaveenPrabakar@users.noreply.github.com>
2024-12-30 18:56:51 -0600

    Update api.js

--- ml-frontend/src/services/api.js
+++ ml-frontend/src/services/api.js
@@ -2,10 +2,10 @@
 import axios from "axios";
 
 export const fetchPlayerStats = (playerName) => 
-  axios.get(`http://127.0.0.1:8000/player-stats/${playerName}`);
+  axios.get(`https://winter-break-project.onrender.com/player-stats/${playerName}`);
 
 export const fetchFullStats = (playerName) => 
-  axios.get(`http://127.0.0.1:8000/player/career/${playerName}`);
+  axios.get(`https://winter-break-project.onrender.com/player/career/${playerName}`);
 
 export const fetchPlotImage = (position, playerName) => {
 
@@ -15,5 +15,5 @@ export const fetchPlotImage = (position, playerName) => {
   const baseUrl = position.toLowerCase() === "quarterback"
     ? "https://winter-break-project.onrender.com"
     : "http://127.0.0.1:8000";
-  return axios.get(`http://127.0.0.1:8000/serve_plot/${playerName}`);
+  return axios.get(`https://winter-break-project.onrender.com/serve_plot/${playerName}`);
 };
